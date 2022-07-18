c856dccdef3527d69c644116dd8b423d9cfe097d
Martin Dubois <martindubois1602@gmail.com>
2022-07-18 18:37:40 +0200

    FightingWithCarouselVue3

--- app/javascript/src/components/Carousel.vue
+++ app/javascript/src/components/Carousel.vue
@@ -1,7 +1,7 @@
 <template>
-  <carousel :items-to-show="1.5">
-    <slide v-for="slide in 10" :key="slide">
-      {{ slide }}
+  <carousel :items-to-show="1">
+    <slide v-for="image in images" :key="image.id">
+      <img src="getImage(image.url)" />
     </slide>
 
     <template #addons>
@@ -24,14 +24,49 @@ export default {
     Pagination,
     Navigation,
   },
+  data() {
+    return {
+      images: [{id: 1,url:'@/assets/images/forhonor1.jpg'}, {id: 2,url:'@/assets/images/forhonor2.jpg'}]
+    }
+  },
+  methods: {
+  getImage(imagePath) {
+    return require(imagePath);
+  }
+}
+
 };
 </script>
 
 <style lang="scss">
 
-.slide {
+#carousel {
   height: 100vh;
-  display: block;
 }
 
+.carousel__item {
+  min-height: 200px;
+  width: 100%;
+  // background-color: var(--vc-clr-primary);
+  color:  var(--vc-clr-white);
+  font-size: 20px;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+}
+
+.carousel__slide {
+  padding: 10px;
+  background-position: 'center';
+  background-size: 'cover';
+  background-repeat: 'no-repeat'
+}
+
+.carousel__prev,
+.carousel__next {
+  box-sizing: content-box;
+  border: 5px solid white;
+}
 </style>
